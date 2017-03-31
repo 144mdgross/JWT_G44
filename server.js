4d764ff7da1768d93e13549107bf09fd42252964
Mathew Brown <matt@mattbrown.com>
2017-03-31 14:51:35 -0600

    Set up session-cookie

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
 
+var cookieSession = require('cookie-session')
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['Gotham','Batman'],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
+
+app.get('/login',(req,res,next) => {
+  // Normally check req.body and verify username/password in DB.
+
+  req.session.jwt = 'insert JWT here';
+
+  res.send();
+});
+
 app.listen('3000',() => {
   console.log('Listening on port 3000');
 })
